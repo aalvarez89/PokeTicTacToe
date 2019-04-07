f02110e1d85578e544baf0d3a202453e5ae63eec
darkstylazz <rezdez89@gmail.com>
2019-04-07 09:52:21 -0400

    Updates

---
+++ precache-manifest.ca984e35f81644c2c7fe0e736853d9d9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "728208b76e903022687121523b84e285",
+    "url": "/PokeTicTacToe/static/media/gr_ex.728208b7.svg"
+  },
+  {
+    "revision": "5e8d0bffd54b2f74c58b",
+    "url": "/PokeTicTacToe/static/css/main.f557517a.chunk.css"
+  },
+  {
+    "revision": "414337af954d41f710c7",
+    "url": "/PokeTicTacToe/static/js/runtime~main.db3588ce.js"
+  },
+  {
+    "revision": "0c4628c1e2e5c879e6c9",
+    "url": "/PokeTicTacToe/static/js/2.2f609849.chunk.js"
+  },
+  {
+    "revision": "371d10e793ba80d0d6c08045c69e6fb5",
+    "url": "/PokeTicTacToe/static/media/gr_oh.371d10e7.svg"
+  },
+  {
+    "revision": "5e8d0bffd54b2f74c58b",
+    "url": "/PokeTicTacToe/static/js/main.44a0adad.chunk.js"
+  },
+  {
+    "revision": "2fa40bf0253e18b1a8d7ccd58efd4b8c",
+    "url": "/PokeTicTacToe/static/media/so_oh.2fa40bf0.svg"
+  },
+  {
+    "revision": "ad0ffbba8fbae7c984da9f01257c724b",
+    "url": "/PokeTicTacToe/static/media/so_ex.ad0ffbba.svg"
+  },
+  {
+    "revision": "0fe52d7900f384e0156b5d8fa4d23a7b",
+    "url": "/PokeTicTacToe/static/media/bu_oh.0fe52d79.svg"
+  },
+  {
+    "revision": "059e322a21b75f62b51d6f981cb46b8b",
+    "url": "/PokeTicTacToe/static/media/bu_ex.059e322a.svg"
+  },
+  {
+    "revision": "23982fd6573c9447910fe535ac6bb432",
+    "url": "/PokeTicTacToe/index.html"
+  }
+];
